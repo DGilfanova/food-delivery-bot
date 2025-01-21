ab9617658112797f063400eea178d95b2d0c878c
Diana Gilfanova <71030843+DGilfanova@users.noreply.github.com>
2025-01-21 21:54:14 +0300

    [DELIVERYCLUB-4] init project (#1)
    Co-authored-by: d.gilfanova <d.gilfanova@vk.team>

---
+++ DeliveryClub/src/main/java/itis/kpfu/ru/deliveryclub/DeliveryClubApplication.java
@@ -0,0 +1,13 @@
+package itis.kpfu.ru.deliveryclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryClubApplication.class, args);
+	}
+
+}
