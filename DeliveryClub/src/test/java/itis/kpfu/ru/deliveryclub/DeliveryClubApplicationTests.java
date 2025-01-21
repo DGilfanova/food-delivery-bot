a3250a570a4f8966b7262acb4ebc78f72654d125
d.gilfanova <d.gilfanova@vk.team>
2025-01-21 21:21:47 +0300

    [DELIVERYCLUB-4] init project

---
+++ DeliveryClub/src/test/java/itis/kpfu/ru/deliveryclub/DeliveryClubApplicationTests.java
@@ -0,0 +1,12 @@
+package itis.kpfu.ru.deliveryclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
